8e583a0a5ef738d6237435adad882a0b5c91a25f
LeoGonzaga <sacredlotus05@gmail.com>
2022-02-13 14:16:54 -0300

    feature: adding subject list by selected option

--- src/screens/Subjects/styles.ts
+++ src/screens/Subjects/styles.ts
@@ -1,3 +1,4 @@
+import { COLORS } from '@themes/colors';
 import styled from 'styled-components';
 
 export const Container = styled.div`
@@ -13,4 +14,25 @@ export const Grid = styled.div`
 export const SubjectContent = styled.div`
   width: 100%;
   margin: 10px;
+`;
+
+export const Table = styled.div`
+  overflow-y: auto;
+  height: 80vh;
+`;
+
+export const TableItem = styled.div`
+  background-color: ${({ theme }) => theme.NEUTRAL};
+  height: 50px;
+  border-radius: 5px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 10px;
+  margin-top: 10px;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.PRIMARY};
+  }
 `;
