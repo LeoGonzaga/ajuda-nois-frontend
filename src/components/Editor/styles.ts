9c6b67646fa2c769eecb28d88ece0c7307277c3a
LeoGonzaga <sacredlotus05@gmail.com>
2022-02-16 23:15:38 -0300

    feature: adding upload file in tiptap editor

--- src/components/Editor/styles.ts
+++ src/components/Editor/styles.ts
@@ -1,3 +1,26 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 10px 20px;
+`;
+
+export const Button = styled.button`
+  background-color: #fff;
+  border: 2px solid #000;
+  border-radius: 4px;
+  padding: 4px;
+  margin-right: 5px;
+  color: #000;
+`;
+
+export const EditorArea = styled.div`
+  margin-top: 10px;
+  border: 1px solid ${({ theme }) => theme.BLACK};
+  padding: 10px;
+  border-radius: 4px;
+
+  div {
+    outline: none;
+    min-height: 500px;
+  }
+`;
