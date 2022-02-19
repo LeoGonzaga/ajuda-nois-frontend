4cf223a6e934aac0151483c080098052369de8fa
LeoGonzaga <sacredlotus05@gmail.com>
2022-02-19 16:51:36 -0300

    feature: creating layout mock quiz

--- src/screens/Quiz/Quiz.tsx
+++ src/screens/Quiz/Quiz.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 
+import EditorContainer from '@components/Editor';
+import TextInput from '@components/Inputs/TextInput';
+import Spacing from '@components/Spacing';
+import Text from '@components/Text';
 import Title from '@components/Title';
 
-import { Container } from './styles';
+import { AwnserContainer, Container } from './styles';
 
 export const Quiz = (): JSX.Element => {
   return (
     <Container>
       <Title text="Iniciar" contrast="quiz" />
+      <Spacing vertical={5} />
+      <Text>
+        Primeiro cadastre as repostas que deseja disponibilizar para a questão.
+        Marque o checkbox para validar a resposta verdadeira.
+      </Text>
+
+      <AwnserContainer>
+        <TextInput placeholder="Digite uma reposta" />
+        <TextInput placeholder="Digite uma reposta" />
+        <TextInput placeholder="Digite uma reposta" />
+        <TextInput placeholder="Digite uma reposta" />
+        <TextInput placeholder="Digite uma reposta" />
+      </AwnserContainer>
+      <EditorContainer />
     </Container>
   );
 };
--- src/screens/Quiz/styles.ts
+++ src/screens/Quiz/styles.ts
@@ -1,6 +1,17 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  height: 100%;
-  padding: 20px;
+  height: 95vh;
+  padding: 20px 10px;
+  overflow-y: auto;
+`;
+
+export const AwnserContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 80%;
+  margin-top: 10px;
+  input {
+    margin-bottom: 10px;
+  }
 `;
