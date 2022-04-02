5aa0d578d681093a936743aa227f4bad067d165c
LeoGonzaga <sacredlotus05@gmail.com>
2022-04-02 19:18:10 -0300

    refactor: chaning structure to render dashboards by role

--- src/screens/Login/components/Form/useForm.ts
+++ src/screens/Login/components/Form/useForm.ts
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
 
 import { yupResolver } from '@hookform/resolvers/yup';
 import { requestAPI, Options } from '@services/index';
 import { handleRedirect } from '@utils/functions';
+import { setUserInfo } from 'src/config/actions/user';
 import { ROUTES } from 'src/routes/routes';
 import * as yup from 'yup';
 
@@ -24,6 +26,7 @@ const schema = yup
   .required();
 
 export const useFormLogin = () => {
+  const dispatch = useDispatch();
   const [loading, setLoading] = useState<boolean>(false);
   const [apiError, setApiError] = useState<boolean>(false);
   const [openNotification, setOpenNotification] = useState<boolean>(false);
@@ -40,19 +43,11 @@ export const useFormLogin = () => {
     setOpenNotification(false);
   };
 
-  const handleRedirectUser = (type: string) => {
-    const redirectByType: any = {
-      admin: handleRedirect(ROUTES.ADMIN),
-      teacher: handleRedirect(ROUTES.TEACHER),
-      student: handleRedirect(ROUTES.HOME),
-    };
-
-    return redirectByType[type];
-  };
-
   const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
     setLoading(true);
     setOpenNotification(false);
+    setApiError(false);
+
     const payload: Options = {
       method: 'POST',
       url: 'login',
@@ -71,24 +66,9 @@ export const useFormLogin = () => {
     }
     const token = response?.data?.token;
     localStorage.setItem('token', token);
-    const usertype = response?.data?.user?.usertype;
-    localStorage.setItem('usertype', usertype);
-
-    setApiError(false);
-    switch (usertype) {
-      case 'admin':
-        handleRedirect(ROUTES.ADMIN);
-        break;
-      case 'teacher':
-        handleRedirect(ROUTES.TEACHER);
-        break;
-      case 'student':
-        handleRedirect(ROUTES.HOME);
-        break;
-
-      default:
-        break;
-    }
+    const user = response?.data?.user;
+    localStorage.setItem('user', JSON.stringify(user));
+    handleRedirect(ROUTES.HOME);
     setLoading(false);
   };
 
