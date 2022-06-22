215900c1daf537ef645a8737aa04896d1f43043c
Leo Gonzaga <sacredlotus05@gmail.com>
2022-06-22 00:09:00 -0300

    feat: adding pomodoro chart

--- src/screens/DashBoard/components/ResultsPomodoro/ResultsPomodoro.tsx
+++ src/screens/DashBoard/components/ResultsPomodoro/ResultsPomodoro.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
+import PieGraph from '@components/PieChart/PieChart';
 import Text from '@components/Text';
 import { COLORS } from '@themes/colors';
 
-import { Container } from './styles';
+import { Circle, Container, Content, Wrapper } from './styles';
 
 export const ResultsPomodoro = (): JSX.Element => {
   return (
@@ -14,6 +15,25 @@ export const ResultsPomodoro = (): JSX.Element => {
         </Text>
         <Text size={14}>Por área do conhecimento</Text>
       </div>
+      <PieGraph />
+      <Content>
+        <Wrapper>
+          <Circle color={COLORS.SUBJECT_BLUE} />
+          <Text>Exatas</Text>
+        </Wrapper>
+        <Wrapper>
+          <Circle color={COLORS.YELLOW} />
+          <Text>Humanas</Text>
+        </Wrapper>
+        <Wrapper>
+          <Circle color={COLORS.RED} />
+          <Text>Linguagem</Text>
+        </Wrapper>
+        <Wrapper>
+          <Circle color={COLORS.GREEN} />
+          <Text>Natureza</Text>
+        </Wrapper>
+      </Content>
     </Container>
   );
 };
