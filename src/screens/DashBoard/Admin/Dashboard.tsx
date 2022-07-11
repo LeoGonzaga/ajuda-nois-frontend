a79d2f7d87b534b0fec1433d7bd7c4475d780279
Bryan Fortunato de Souza <bryanf@comprovei.com>
2022-07-11 10:11:38 -0300

    Fix: dash faltando

---
+++ src/screens/DashBoard/Admin/Dashboard.tsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+import { FaUsers, FaChalkboardTeacher, FaBook } from 'react-icons/fa';
+
+import SecondaryButton from '@components/Buttons/SecondaryButton';
+import Spacing from '@components/Spacing';
+import Title from '@components/Title';
+import { COLORS } from '@themes/colors';
+import { AREAS } from '@utils/functions';
+import Router from 'next/router';
+import { ROUTES } from 'src/routes/routes';
+
+import AdminChart from '../components/AdminChart';
+import BigCard from '../components/BigCard';
+import StudentTable from './StudentTable';
+import { Column, Container, Controls, Row } from './styles';
+import TeacherTable from './TeacherTable';
+
+export const Dashboard = ({ data }: any): JSX.Element => {
+  const [value, setValue] = useState();
+  const handleRedirectToPanel = () => {
+    Router.push(ROUTES.PANEL);
+  };
+
+  const handleGetSubjectsByTeacher = () => {
+    const content = data?.newTeachers?.map((teacher: any) => {
+      const areas = teacher?.subjects_info?.map(
+        (subject: any) => AREAS[subject.area]
+      );
+      const subjects = teacher?.subjects_info?.map(
+        (subject: any) => subject.name
+      );
+      return {
+        username: teacher.username,
+        email: teacher.email,
+        fields: areas.toString(),
+        subjects: subjects.toString(),
+      };
+    });
+
+    setValue(content);
+  };
+
+  useEffect(() => {
+    if (data) {
+      handleGetSubjectsByTeacher();
+    }
+  }, [data]);
+  return (
+    <Container>
+      <Controls>
+        <Title text="Dashboard" contrast="" subText="" />
+        <SecondaryButton onClick={handleRedirectToPanel}>
+          Painel de controle
+        </SecondaryButton>
+      </Controls>
+      <Column>
+        <Row>
+          <BigCard
+            color={COLORS.SUBJECT_BLUE}
+            indicator="Alunos"
+            value={data?.allStudents}
+            icon={<FaUsers />}
+          />
+          <Spacing horizontal={10} />
+          <BigCard
+            color={COLORS.GREEN}
+            indicator="Professores"
+            value={data?.allTeachers}
+            icon={<FaChalkboardTeacher />}
+          />
+          <Spacing horizontal={10} />
+          <BigCard
+            color={COLORS.RED}
+            indicator="Simulados"
+            value={data?.allMockExam}
+            icon={<FaBook />}
+          />
+        </Row>
+        <Spacing vertical={5} />
+        <AdminChart />
+        <Spacing vertical={10} />
+        <StudentTable size={10} data={data?.newStudents} />
+        <Spacing vertical={10} />
+        <TeacherTable size={3} data={value} />
+      </Column>
+    </Container>
+  );
+};
---
+++ src/screens/DashBoard/Admin/TeacherTable/TeacherTable.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect } from 'react';
+
+import Spacing from '@components/Spacing';
+
+import Expandable from '../../components/Expandable';
+import { Container, Content } from './styles';
+type Props = {
+  size: number;
+  data: any;
+};
+
+export const TeacherTable = ({ size, data }: Props): JSX.Element => {
+  const dataSorted = data?.sort((a: any, b: any) =>
+    a.username.localeCompare(b.username)
+  );
+
+  useEffect(() => {
+    size = dataSorted?.length;
+  }, []);
+
+  return (
+    <Container>
+      <Expandable
+        title="Professores"
+        subtitle="Cadastrados neste ano"
+        size={dataSorted?.length}
+        simSize={0}
+        type="admin"
+      >
+        <Spacing vertical={10} />
+        <Content>
+          <table>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Áreas</th>
+                <th>Disciplinas</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {dataSorted?.map(
+                ({ username, email, fields, subjects }: any, index: number) => {
+                  return (
+                    <tr key={index}>
+                      <td>{username}</td>
+                      <td>{email}</td>
+                      <td>{fields}</td>
+                      <td>{subjects}</td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+          </table>
+        </Content>
+      </Expandable>
+    </Container>
+  );
+};
